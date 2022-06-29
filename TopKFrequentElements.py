ebab51877dbd8b83cf68663ea80bcfc56191f5ac
lzhong90 <76064127+lzhong90@users.noreply.github.com>
2022-06-29 11:20:10 +0200

    Update TopKFrequentElements.py
    add method 2

--- TopKFrequentElements.py
+++ TopKFrequentElements.py
@@ -6,3 +6,13 @@ class Solution(object):
         :rtype: List[int]
         """
         return [word for word, word_count in Counter(nums).most_common(k)]
+    
+    ###Method 2###
+       dic = {}
+        for i in nums:
+            if i in dic:
+                dic[i] += 1
+            else:
+                dic[i] = 1
+
+        return sorted(dic, key = dic.get, reverse = True)[:k]
