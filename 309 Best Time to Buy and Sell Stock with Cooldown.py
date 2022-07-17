e5e2baeaa87c8b604ee184c234388dabadb2c587
lzhong90 <76064127+lzhong90@users.noreply.github.com>
2022-07-17 21:04:20 +0200

    Create 309 Best Time to Buy and Sell Stock with Cooldown.py

---
+++ 309 Best Time to Buy and Sell Stock with Cooldown.py
@@ -0,0 +1,22 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        # everyday there are in total three possible states: hold 0, sold 1, rest 2
+        # we need to maximize the value of each possible state on ith day
+        # hold[i] = max(hold[i-1], rest[i-1] - price)
+        # sold[i] = hold[i-1] + price
+        # rest[i] = max(sold[i-1], rest[i-1])
+        
+        
+        
+        l = len(prices)
+        hold = [float('-inf')] * (l+1)
+        sold = [0]*(l+1)
+        rest = [0]*(l+1)
+        
+        for i in range(1, l+1):
+            hold[i] = max(hold[i-1], rest[i-1] -prices[i-1])
+            sold[i] = hold[i-1] + prices[i-1]
+            rest[i] = max(sold[i-1], rest[i-1])
+        
+        return max(sold[l], rest[l])
+            
